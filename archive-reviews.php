228ee41733af7014e55490e7926f6411693e8903
BerkantA21 <berkant5@hotmail.com>
2022-05-19 12:22:21 +0200

    19-5-2022

--- archive-reviews.php
+++ archive-reviews.php
@@ -1,1 +1,8 @@
-tytfyhyydhd
+<?php
+
+$title = 'reviews archief';
+
+$arguments = ['post_type' => 'reviews', 'numberposts' => 5, 'category' => 0, 'orderby' => 'date', 'order' => 'DESC'];
+$newsItemCollection = get_posts($arguments);
+
+render('views/templates/reviews-archive.php', compact('title', 'newsItemCollection'));
